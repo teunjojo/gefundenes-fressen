091825097d15dde1b0c968483ebcb1a511e2d6cc
teunjojo <mail@teunjojo.com>
2022-09-18 20:23:48 +0200

    Add Recipe Model

---
+++ Eindwerkstuk/Eindwerkstuk/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eindwerkstuk.Models
+{
+    public class Recipe
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+    }
+}
