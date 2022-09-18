54ec619d424dc48b686a9322f1b0c502e9d13d79
teunjojo <mail@teunjojo.com>
2022-09-18 20:30:30 +0200

    Add SearchPage

---
+++ Eindwerkstuk/Eindwerkstuk/Views/SearchPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Eindwerkstuk.ViewModels;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Eindwerkstuk.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchPage : ContentPage
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
