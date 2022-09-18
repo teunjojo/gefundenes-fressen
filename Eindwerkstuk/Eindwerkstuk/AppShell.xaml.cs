b3f0c3458bbabb006f0fdf60706a337f00ee4448
teunjojo <mail@teunjojo.com>
2022-09-18 20:30:03 +0200

    Delete NewItemPage

--- Eindwerkstuk/Eindwerkstuk/AppShell.xaml.cs
+++ Eindwerkstuk/Eindwerkstuk/AppShell.xaml.cs
@@ -12,7 +12,6 @@ namespace Eindwerkstuk
         {
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
     }
