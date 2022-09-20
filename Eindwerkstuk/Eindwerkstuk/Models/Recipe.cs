6565700952e0ea58da593ac01515be0ffc94e0aa
teunjojo <mail@teunjojo.com>
2022-09-20 21:10:09 +0200

    Clean up Recipe.cs

--- Eindwerkstuk/Eindwerkstuk/Models/Recipe.cs
+++ Eindwerkstuk/Eindwerkstuk/Models/Recipe.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Eindwerkstuk.Models
 {
     public class Recipe
     {
         public string Id { get; set; }
-        public string Text { get; set; }
+        public string Name { get; set; }
         public string Image { get; set; }
     }
 }
