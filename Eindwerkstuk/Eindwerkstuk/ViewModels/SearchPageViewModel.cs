ec1fd8a94291dac0aa92ec2dbfe3f8af584dad7f
teunjojo <mail@teunjojo.com>
2022-09-20 21:10:45 +0200

    Fix SearchPageViewModel.cs

---
+++ Eindwerkstuk/Eindwerkstuk/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Eindwerkstuk.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Eindwerkstuk.ViewModels
+{ 
+    public class SearchPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<Recipe> Recipes { get; }
+        public SearchPageViewModel()
+        {
+            Title = "Zoeken";
+            Recipes = new ObservableCollection<Recipe>();
+            Recipes.Add(new Recipe() { Id = Guid.NewGuid().ToString(), Name = "How To Make Classic French Toast", Image = "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/341495.jpg" });
+        }
+    }
+}
